d3060ba65141e60f74205d2e360e6f471229529c
JiHee <kih3193@naver.com>
2025-10-22 17:39:39 +0900

    d

--- 1차_웹프로젝트/03.구현소스(디자인&소스)/03.-1구현코드/js/전시.js
+++ 1차_웹프로젝트/03.구현소스(디자인&소스)/03.-1구현코드/js/전시.js
@@ -16,6 +16,22 @@ R: Song of a Manhattan Suicide Addict 2010
 After relocating to the United States in 1957, Kusama gained a reputation for her net paintings and sculptures that embody ‘self-obliteration’: the feeling of losing the boundary between the self and the other through the obsessive repetition of motifs originating from her hallucinations. In her anti-war happenings in the late 1960s, she painted dots onto the human body using the same concept of ‘self-obliteration’ while also highlighting the beauty of life and the human body. During the 1970s and 80s, following the loss of her father and her lover, as well as her return to Japan due to health issues, Kusama produced numerous dark-toned collages and three-dimensional works centered on the theme of death, as well as poetry and novels imbued with a sense of mortality. As she continued creating fantastical works exploring death and the afterlife, her works from the late 1980s began to explore themes of transmigration and cyclical returns to eternity through ‘self-obliteration’. Kusama’s works, which increasingly incorporated more colors, reveal how her creative process evolved from a means of coping with death to becoming synonymous with her very existence. In her painting series from 2000 onwards, Kusama has been relentlessly depicting the beauty of life and the joy of living on canvases overflowing with vibrant colors, driven by the ever-looming presence of her own death.`
  
  },
+   { index: 2,
+    title: "I WOULD OVERCOME DEATH AND GO ON LIVING",
+    imgtxt1: `L: Lingering Dream 1949
+© YAYOI KUSAMA
+R: Kusama with her soft sculpture works at the solo exhibition Yayoi Kusama: Obsessional Art: A Requiem for Death and Life, Painting, Sculpture and Objects 1976
+Photo:Shigeo Anzaï
+©Estate of Shigeo Anzaï, Courtesy of Zeit-Foto`,
+    imgtxt2: `L: Flame of Life- Dedicated to Tu-Fu Private Collection 1988
+R: Song of a Manhattan Suicide Addict 2010
+© YAYOI KUSAMA`,
+    imgtxt3: "",
+    exbtxt: `Yayoi Kusama has constantly faced the critical realities of life and death as pressing issues. Her experience of the Pacific War in a complex family environment, along with her overcoming of suicidal impulses triggered by trauma and neurosis through her creative practices, has influenced her perception of these issues. This exhibition unveils Kusama’s evolving outlook on life and death, alongside the corresponding shifts in her artistic presentation, through a series of diverse works—from her 1940s and 1950s paintings, which bear the imprint of war, to her very latest pieces.
+
+After relocating to the United States in 1957, Kusama gained a reputation for her net paintings and sculptures that embody ‘self-obliteration’: the feeling of losing the boundary between the self and the other through the obsessive repetition of motifs originating from her hallucinations. In her anti-war happenings in the late 1960s, she painted dots onto the human body using the same concept of ‘self-obliteration’ while also highlighting the beauty of life and the human body. During the 1970s and 80s, following the loss of her father and her lover, as well as her return to Japan due to health issues, Kusama produced numerous dark-toned collages and three-dimensional works centered on the theme of death, as well as poetry and novels imbued with a sense of mortality. As she continued creating fantastical works exploring death and the afterlife, her works from the late 1980s began to explore themes of transmigration and cyclical returns to eternity through ‘self-obliteration’. Kusama’s works, which increasingly incorporated more colors, reveal how her creative process evolved from a means of coping with death to becoming synonymous with her very existence. In her painting series from 2000 onwards, Kusama has been relentlessly depicting the beauty of life and the joy of living on canvases overflowing with vibrant colors, driven by the ever-looming presence of her own death.`
+ 
+ },
 
 
 
